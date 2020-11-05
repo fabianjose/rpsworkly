1ed47a014a66d411b030cdc339644ce70fac6c1f
fabianjose <fabian@remotepcsolutions.com>
2020-11-05 14:24:47 -0500

    Revert "no se que verga pasa"
    This reverts commit 1ca9581d94c7b652554a9d27836e9086c4a28767.

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const path = require('path')
+const app = express()
+const morgan= require('morgan')
+const mysql = require('mysql')
+const myconnection = require('express-myconnection')
+
+//importando rutas
+const usuariosRoutes = require('./routes/usuarios')
+
+// configuraciones
+app.set('port', process.env.PORT || 3000)
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+
+//middleware
+app.use(morgan('dev'))
+app.use(myconnection(mysql,{
+    host:'localhost',
+    user:"root",
+    password:"",
+    port: 3306,
+    database:"workly"
+},'single'))
+app.use(express.urlencoded({extends:false}))
+
+//routes
+
+app.use ("/",usuariosRoutes)
+
+//static files
+app.use(express.static(path.join(__dirname,'public')))
+
+//conexion
+
+
+//iniciando el servidor
+app.listen(app.get('port'), ()=> {
+    console.log("server on port 3000");
+})
---
+++ src/routes/usuarios.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const usuariosController = require("../controller/usuariosController")
+var md_auth = require('../middlewares/authenticated');
+router.get('/',usuariosController.list)
+
+router.get('/',usuariosController.home )
+router.get('/registro',usuariosController.registro )
+router.get('/Crear_vacante',usuariosController.crear_vacante )
+router.get('/login',usuariosController.login )
+router.get('/list',usuariosController.list )
+
+router.post('/add',usuariosController.save)
+router.get('/delete/:id',usuariosController.delete)
+router.get('/update/:id', md_auth.authenticated, usuariosController.update)
+router.post('/update/:id',usuariosController.saveUpdate)
+
+router.post('/login',usuariosController.signIn);
+
+module.exports = router;
