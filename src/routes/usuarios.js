4baedace95d28d366feedecd4e7805f2b263b130
simon jose aguilera gomez <simonfilth86@hotmail.com>
2020-11-05 21:02:25 -0400

    correccion de bug de ruta home y list

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const usuariosController = require("../controller/usuariosController")
+const usuariosController = require("../controller/usuariosController");
 var md_auth = require('../middlewares/authenticated');
-router.get('/',usuariosController.list)
+router.get('/lista',usuariosController.list);
 
-router.get('/',usuariosController.home )
-router.get('/registro',usuariosController.registro )
-router.get('/Crear_vacante',usuariosController.crear_vacante )
-router.get('/login',usuariosController.login )
-router.get('/list',usuariosController.list )
+router.get('/',usuariosController.home);
+router.get('/registro',usuariosController.registro);
+router.get('/Crear_vacante',usuariosController.crear_vacante);
+router.get('/login',usuariosController.login);
 
-router.post('/add',usuariosController.save)
-router.get('/delete/:id',usuariosController.delete)
-router.get('/update/:id', md_auth.authenticated, usuariosController.update)
-router.post('/update/:id',usuariosController.saveUpdate)
+router.post('/add',usuariosController.save);
+router.get('/delete/:id',usuariosController.delete);
+router.get('/update/:id', md_auth.authenticated, usuariosController.update);
+router.post('/update/:id',usuariosController.saveUpdate);
 
 router.post('/login',usuariosController.signIn);
 
